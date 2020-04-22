e123fd7274ef8c1943d0cb84d2a3f0475b726c19
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-04-22 10:06:20 -0500

    ci(nox): add safety function to check the python dependencies for known vulnerabilities

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
+import tempfile
+
 import nox
 
 # Excluding black from the sessions run by default
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 
 
 @nox.session(python=["3.8", "3.7", "3.6"])
@@ -32,3 +34,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
