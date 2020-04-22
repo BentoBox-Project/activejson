c069618f208d13bb527c5288e4e227c6f6dfa83d
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-04-22 08:43:07 -0500

    style(noxfile): add a section to run flake8 in noxfile

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,13 @@ def tests(session):
     args = session.posargs or ["--cov", "-vv"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+locations = "activejson", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8", "3.7", "3.6"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
